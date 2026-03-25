e4f70bb76f3d5d7633c905e7828585ebd3ff1feb
munununu <hunyeongm@gmail.com>
2026-03-25 12:51:43 +0900

    feat: spring 프로젝트 병합

---
+++ ai-meal-assistant-backend/src/main/java/capstone/ai_meal_assistant_backend/AiMealAssistantBackendApplication.java
@@ -0,0 +1,13 @@
+package capstone.ai_meal_assistant_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiMealAssistantBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiMealAssistantBackendApplication.class, args);
+	}
+
+}
